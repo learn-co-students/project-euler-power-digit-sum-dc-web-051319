6fad144b963a32a9d63862d4bb25b28c8e586620
Matt Williams <matt.seongjun@gmail.com>
2019-08-20 09:14:23 -0400

    Done

--- lib/project_euler_power_digit_sum.rb
+++ lib/project_euler_power_digit_sum.rb
@@ -1,1 +1,7 @@
-# code your solution here
+def power_digit_sum(x, n)
+    power = x ** n
+    digits = power.to_s.split('')
+    digits.inject{ |sum, d| sum.to_i + d.to_i }
+end
+
+puts power_digit_sum(2, 4)
